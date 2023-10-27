4c2543589fd2eab00e560b7a50fd7125463e09a0
Israel Kilday <israel.kilday@yahoo.com.br>
2023-10-26 22:13:30 -0300

    create data rescue hook

--- src/hooks/useFetchDocuments.jsx
+++ src/hooks/useFetchDocuments.jsx
@@ -3,7 +3,7 @@ import { db } from "../firebase/config";
 import {
     collection,
     query,
-    orderby,
+    orderBy,
     onSnapshot,
     where,
 } from "firebase/firestore";
@@ -12,4 +12,51 @@ export const useFetchDocument = (docCollection, search = null, uid = null) => {
     const [documents, setDocuments] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(null);
+
+    // deal memory leak
+    const [cancelled, setCancelled] = useState(false);
+
+    useEffect(() => {
+        async function loadData() {
+            if (cancelled) return;
+
+            setLoading(true);
+
+            const collectionRef = await collection(db, docCollection)
+
+            try {
+                let q;
+
+                // busca
+                // dashboard
+
+                q = await query(collection, orderBy("createdAt", "desc"));
+
+                await onSnapshot(q, (querySnapshot) => {
+                    setDocuments(
+                        querySnapshot.docs.map((doc) => ({
+                            id: doc.id,
+                            ...doc.data(),
+                        }))
+                    );
+                });
+
+                setLoading(false);
+
+            } catch (error) {
+                console.log(error);
+                setError(error.message);
+
+                setLoading(false);
+            };
+        }
+
+        loadData();
+    }, [docCollection, search, uid, cancelled])
+
+    useEffect(() => {
+
+    }, []);
+
+    return { documents, loading, error };
 }
