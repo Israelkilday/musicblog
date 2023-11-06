49e5db511e40a0ede5aa60c63b3bb18087b2a875
Israel Kilday <israel.kilday@yahoo.com.br>
2023-11-05 23:36:50 -0300

    working in responsive menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,76 +9,84 @@ import { userAuthValue } from "../context/AuthContext";
 
 const Navbar = () => {
     const { user } = userAuthValue();
-    
+
     const { logout } = useAuthentication();
 
     return (
-        <nav className={styles.navbar}>
-            <NavLink to="/" className={styles.brand}>
-                Mini <span>Blog</span>
-            </NavLink>
+        <div className={styles.header}>
+            <nav className={styles.navbar}>
+                <NavLink to="/" className={styles.brand}>
+                    Mini <span>Blog</span>
+                </NavLink>
 
-            <ul className={styles.links_list}>
-                <li>
-                    <NavLink
-                        to="/"
-                        className={({ isActive }) => (isActive ? styles.active : "")}
-                    >
-                        Home
-                    </NavLink>
-                </li>
-
-                {!user && (
-                    <>
-                        <li>
-                            <NavLink to="/login"
-                                className={({ isActive }) => (isActive ? styles.active : "")}
-                            >Entrar
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/register"
-                                className={({ isActive }) => (isActive ? styles.active : "")}
-                            >Cadastrar
-                            </NavLink>
-                        </li>
-                    </>
-                )}
-
-                {user && (
-                    <>
-                        <li>
-                            <NavLink to="/posts/create"
-                                className={({ isActive }) => (isActive ? styles.active : "")}
-                            >Novo Post
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/dashboard"
-                                className={({ isActive }) => (isActive ? styles.active : "")}
-                            >Dashboard
-                            </NavLink>
-                        </li>
-                    </>
-                )}
-
-                <li>
-                    <NavLink
-                        to="/about"
-                        className={({ isActive }) => (isActive ? styles.active : "")}
-                    >
-                        Sobre
-                    </NavLink>
-                </li>
-
-                {user && (
+                <ul className={styles.links_list}>
                     <li>
-                        <button onClick={logout}> Sair</button>
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) => (isActive ? styles.active : "")}
+                        >
+                            Home
+                        </NavLink>
                     </li>
-                )}
-            </ul>
-        </nav>
-    )
-}
 
-export default Navbar
+                    {!user && (
+                        <>
+                            <li>
+                                <NavLink to="/login"
+                                    className={({ isActive }) => (isActive ? styles.active : "")}
+                                >Entrar
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/register"
+                                    className={({ isActive }) => (isActive ? styles.active : "")}
+                                >Cadastrar
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
+
+                    {user && (
+                        <>
+                            <li>
+                                <NavLink to="/posts/create"
+                                    className={({ isActive }) => (isActive ? styles.active : "")}
+                                >Novo Post
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard"
+                                    className={({ isActive }) => (isActive ? styles.active : "")}
+                                >Dashboard
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
+
+                    <li>
+                        <NavLink
+                            to="/about"
+                            className={({ isActive }) => (isActive ? styles.active : "")}
+                        >
+                            Sobre
+                        </NavLink>
+                    </li>
+
+                    {user && (
+                        <li>
+                            <button onClick={logout}> Sair</button>
+                        </li>
+                    )}
+                </ul>
+            </nav>
+
+            <button id="menu_icon" className={styles.btn_menu}>
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+            </button>
+        </div>
+    );
+};
+
+export default Navbar;
