8f55f333f422fd0e720b5859fca7db2551ba92d3
Israel Kilday <israel.kilday@yahoo.com.br>
2023-11-21 12:29:57 -0300

    working on reponsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import styles from "./Footer.module.css"
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <h3>Escreva sobre o quê voce tem interesse!</h3>
-      <p>Mini Blog &copy; 2023</p>
-    </footer>
-  )
-}
+      <footer className={styles.footer}>
+        <h3>Escreva sobre o quê voce tem interesse!</h3>
+        <p>Mini Blog &copy; 2023</p>
+      </footer>
+  );
+};
 
 export default Footer;
