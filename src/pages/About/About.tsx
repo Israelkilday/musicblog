7cfee3bcbf6e8be843e0582a152a90add7475622
Israel Kilday <israel.kilday@yahoo.com.br>
2023-12-15 13:41:23 -0300

    changing p from about again

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -12,7 +12,7 @@ const About = () => {
                 <p>
                     Este é um projeto pessoal, uma aplicação web construída com React, integrada ao Firebase para criar, editar e visualizar Posts.
                     O objetivo é criar Posts falando de música em geral, aproveitei para iniciar o Blog postando sobre algumas de minhas bandas favoritas, mais sinta-se a vontade para postar sobre qualquer estilo musical.
-                    Para facilitar o acesso faça login com o usuário: <span className={styles.italic}>usuarioTeste@email.com</span>, senha: <span className={styles.italic}>123456</span> Assim poderá ter acesso ao Dashboard. Quando escolher uma imagem para criar um Post, clique nela com o botão direito e copie seu endereço para que seja uma URL valida, Obrigado pela visita!
+                    Para facilitar o acesso faça login com o usuário: <span className={styles.italic}>usuarioTeste@email.com</span>, senha: <span className={styles.italic}>123456</span> Assim poderá ter acesso ao Dashboard. Quando escolher uma imagem para criar um Post, clique nela com o botão direito e copie seu endereço para que seja uma URL valida, <br/>Obrigado pela visita!
                 </p>
             </div>
             <Link to="/posts/create" className="btn">Criar Post</Link>
