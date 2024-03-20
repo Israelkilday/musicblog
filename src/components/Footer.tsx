8b2d0d5cc3613658d5aa5e6b3038ea4d786ef7e4
Israel Kilday <israel.kilday@yahoo.com.br>
2024-03-20 00:50:29 -0300

    style: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,35 +4,83 @@ import styles from "./Footer.module.css"
 import { NavLink } from "react-router-dom";
 // REACT ICONS
 import { MdEmail } from "react-icons/md";
-import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaInstagram, FaArrowUp, FaWhatsapp, FaPhoneAlt } from "react-icons/fa";
+import { HiMiniPlayPause } from "react-icons/hi2";
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h3>Escreva sobre sua banda favorita!</h3>
+      <h3 className={styles.h3_footer}>ESCREVA SOBRE A SUA BANDA FAVORITA!</h3>
+      <div className={styles.box_footer}>
+        <div className={styles.box_footer_icons} >
 
-      <div className={styles.box_footer_icons} >
-          
-        <div className={styles.footer_icons}>
-          <NavLink to="https://www.linkedin.com/in/israeldevfrontend">
-            <FaLinkedin className={styles.linkedin} />
+          <NavLink to="/" className={styles.brand}>
+            <HiMiniPlayPause className={styles.icon} />
+            Music <span>Blog</span>
+            <HiMiniPlayPause className={styles.icon2} />
           </NavLink>
 
-          <NavLink to="https://github.com/Israelkilday">
-            <FaGithub className={styles.github} />
-          </NavLink>
+          <div className={styles.footer_icons}>
+            <NavLink to="https://www.linkedin.com/in/israeldevfrontend">
+              <FaLinkedin className={styles.linkedin} />
+            </NavLink>
 
-          <NavLink to="https://www.instagram.com/israelkilday/">
-            <FaInstagram className={styles.instagram} />
-          </NavLink>
+            <NavLink to="https://github.com/Israelkilday">
+              <FaGithub className={styles.github} />
+            </NavLink>
 
-          <a href="mailto:israel.kilday@yahoo.com.br">
-            <MdEmail className={styles.email} />
-          </a>
+            <NavLink to="https://www.instagram.com/israelkilday/">
+              <FaInstagram className={styles.instagram} />
+            </NavLink>
+
+            <a href="mailto:israel.kilday@yahoo.com.br">
+              <MdEmail className={styles.email} />
+            </a>
+          </div>
+        </div>
+
+        <div >
+          <h3>INSTITUCIONAL</h3>
+          <p>FAQs</p>
+          <p>Sobre o MusicBlog</p>
+          <p>Política de segurança e privacidade</p>
+        </div>
+
+        <div >
+          <h3>DEPARTAMENTOS</h3>
+          <p>Forums</p>
+          <p>Afiliados</p>
+          <p>Newsletter</p>
+        </div>
+
+        <div>
+          <h3>CONTATO</h3>
+          <p className={styles.p_icon}>
+            <FaWhatsapp className={styles.icon} />
+            (85) 912345678
+          </p>
+          <p className={styles.p_icon}>
+            <FaPhoneAlt className={styles.icon_phone} />
+            SAC 0800 1234 5678
+          </p>
+          <p>www.MusicBLOG@email.com</p>
         </div>
       </div>
 
-      <p>Criado por Israel Kilday &copy; 2023</p>
+      <div className={styles.end_footer}>
+        <p className={styles.p_end_footer}>&copy; 2023 ISRAEL KILDAY<br/> Todos os direitos reservados</p>
+
+        <div onClick={scrollToTop} className={styles.arrow_up}>
+          <FaArrowUp />
+        </div>
+      </div>
     </footer>
   );
 };
